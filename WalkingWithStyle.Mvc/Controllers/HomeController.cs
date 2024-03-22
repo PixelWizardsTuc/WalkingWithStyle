82bc424241ddf713199f46ab9920755474e644bc
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-03-22 12:04:01 +0100

    fixed the shoes page with adding some homecontrollercode.

--- WalkingWithStyle.Mvc/Controllers/HomeController.cs
+++ WalkingWithStyle.Mvc/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace WalkingWithStyle.Mvc.Controllers
             return View();
         }
 
+        public IActionResult Shoes()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
